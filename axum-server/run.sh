a9a30d991097c76262053ed96d335854935b780a
myyrakle <sssang97@naver.com>
2024-02-26 01:56:41 +0900

    axum server 코드 및 dockerfile 수정

--- axum-server/run.sh
+++
@@ -1,4 +0,0 @@
-# !/bin/sh
-cd /home 
-ls -la
-./app
--- axum-server/src/main.rs
+++ axum-server/src/main.rs
@@ -12,9 +12,7 @@ async fn main() {
     let app = Router::new().route("/", get(handler));
 
     // run it
-    let listener = tokio::net::TcpListener::bind("127.0.0.1:3000")
-        .await
-        .unwrap();
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:80").await.unwrap();
     println!("listening on {}", listener.local_addr().unwrap());
     axum::serve(listener, app).await.unwrap();
 }
